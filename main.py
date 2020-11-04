43bb7c059e08a47879cdb33ed081af2721ab8774
HumbleCatcher <andreas.ellison@gmail.com>
2020-11-04 22:07:30 +0100

    Save simulation data and use matplotlib to show draw graphs (population and traits)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from application.application import Application
-from models.simulation import Simulation
 
 if __name__ == '__main__':
     application = Application(window_width=600, window_height=400)
